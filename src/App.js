cd05dc949768883f547279f9f92fcdff48e7a02b
Dukewendo <dommct@yahoo.co.uk>
2023-01-15 22:27:43 +0000

    state testing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React from "react";
-import { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react';
 
-function App() {
-  const [isGoingOut, setIsGoingOut] = useState(true);
-function changeMind() {
-setIsGoingOut(prevState => !prevState)
+export default function App() {
+
+    const [loveWillys, setLoveWillys] = useState();
+
+    function changeWillys() {
+        setLoveWillys( prevState => !prevState)
+    }
+
+
+    return(
+        <div>
+            <h1>Do you love willys?</h1>
+            <h2 onClick={changeWillys}>{loveWillys ? "Yes" : "Nope"}</h2>
+        </div>
+    )
 }
-
-  return (
-    <div className="state">
-      <h1 className="state--title">Do I go out tonight?</h1>
-      <div onClick={changeMind} className="state--value">
-        <h1>{isGoingOut ? "Yes" : "Nope"}</h1>
-      </div>
-    </div>
-  );
-}
-
-export default App;

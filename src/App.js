3e25d2f2d69b9cfbe99f4f37b74a63d4694be57e
Dukewendo <dommct@yahoo.co.uk>
2023-01-18 16:48:10 +0000

    updating state

--- src/App.js
+++ src/App.js
@@ -6,22 +6,34 @@ export default function App() {
     lastName: "Pills",
     phone: "+1 (709) 555-1212",
     email: "thisismyrealemail.com",
-    isFavouirte: false
+    isFavouirte: false,
   });
 
-let starIcon = contact.isFavouirte ? "star-filled.png" : "star-empty.png"
+  let starIcon = contact.isFavouirte ? "star-filled.png" : "star-empty.png";
 
-function toggleFavourote() {
-console.log("Checked")
-}
+  function toggleContact() {
+    setContact((prevContact) => {
+      return {
+        ...prevContact,
+        isFavouirte: !prevContact.isFavouirte,
+      };
+    });
+  }
 
   return (
     <main>
       <article className="card">
-        <img className="card--image" src="" alt="" />
+        <img className="card--image" src="" alt="card" />
         <div>
-          <img className="card--favourite" src={`./images/{starIcon}`} alt="" />
-          <h2 className="card--name">{contact.firstName} {contact.lastName}</h2>
+          <img
+            className="card--favourite"
+            src={`./images/{starIcon}`}
+            alt="star"
+            onClick={toggleContact}
+          />
+          <h2 className="card--name">
+            {contact.firstName} {contact.lastName}
+          </h2>
           <p className="card--contact">{contact.phone}</p>
           <p className="card--contact">{contact.email}</p>
         </div>
